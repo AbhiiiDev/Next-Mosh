05c2fb44bc31129a774180f6674345f009f113af
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-03-16 00:05:44 +0530

    FEAT: Added custom not found page

---
+++ next-app/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      Can't find the page , you requested , Sorry!!
+      <div>
+        Gooooochi Gooooochi Guuuuuuuuu
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
