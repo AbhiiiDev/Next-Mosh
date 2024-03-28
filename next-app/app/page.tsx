835e60d29b693a4c1ca4e29848fba278b1804c49
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-03-28 11:03:14 +0530

    Accessed session on server side

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,19 @@
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+
+const session= await getServerSession(authOptions)
   return (
  <div>
-  YO man 
+  YO man,   
+  <span className="font-bold">
+
+  {
+    session && session.user?.name
+  }
+  </span>
  </div>
   );
 }
